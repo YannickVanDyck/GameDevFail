8e71a6dcba39feb1a231775ed562eebecb8aba5b
Yannick <vandyckyannick@hotmail.com>
2019-12-02 11:26:50 +0100

    Collision 1.4

--- Project-YannickVanDyck/CollisionManager.cs
+++ Project-YannickVanDyck/CollisionManager.cs
@@ -5,6 +5,12 @@ namespace Project_YannickVanDyck
 {
     class CollisionManager
     {
+        bool stopFall;
+        bool stopLeft;
+        bool stopRight;
+        bool stopJump;
+
+
         List<ICollideBlok> Collection;
         Hero Hero;
         //hier elk blokje in de lijst Collection controleren op collision met hero
@@ -22,19 +28,30 @@ namespace Project_YannickVanDyck
                 if (Hero.CollisionRectangleTop.Intersects(item.CollisionRectangleTop))
                 {
                     Console.WriteLine("Stop met vallen");
+                    stopFall = true;
                 }
+                else stopFall = false;
+
                 if (Hero.CollisionRectangleTop.Intersects(item.CollisionRectangleBottom))
                 {
                     Console.WriteLine("Stop met springen");
+                    stopJump = true;
                 }
+                else stopJump = false;
+
                 if (Hero.CollisionRectangleTop.Intersects(item.CollisionRectangleRight))
                 {
                     Console.WriteLine("Stop met naar links te bewegen");
+                    stopLeft = true;
                 }
+                else stopLeft = false;
+
                 if (Hero.CollisionRectangleTop.Intersects(item.CollisionRectangleLeft))
                 {
                     Console.WriteLine("Stop met naar Rechts te bewegen");
+                    stopRight = true;
                 }
+                else stopRight = false;
             }
         }
         #endregion
